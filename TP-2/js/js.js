1afcf3fc9c98cf2cffdd7af181a2d08a2b6775a2
Luca Ibarra <ibarraluca4@gmail.com>
2023-10-09 20:03:18 -0300

    cambie el login

--- TP-2/js/js.js
+++ TP-2/js/js.js
@@ -54,3 +54,18 @@ function ocultarLoading(){
   load.classList.add("hiden")
 }
 
+
+
+/*******login***********/
+
+const signUpButton = document.getElementById('signUp');
+const signInButton = document.getElementById('signIn');
+const container = document.getElementById('containerl');
+
+signUpButton.addEventListener('click', () => {
+	container.classList.add("right-panel-active");
+});
+
+signInButton.addEventListener('click', () => {
+	container.classList.remove("right-panel-active");
+});
